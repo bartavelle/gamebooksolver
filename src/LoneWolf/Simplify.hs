a52cc9044e929bf4bd93cf2c69f25d4117abfd1a
bartavelle <bartavelle@gmail.com>
2017-03-25 08:10:48 +0100

    Fix wrong simplification of Randomly

--- src/LoneWolf/Simplify.hs
+++ src/LoneWolf/Simplify.hs
@@ -6,14 +6,32 @@ import LoneWolf.Rules
 
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import Data.Monoid
 import Control.Lens
+
+newtype ChapterSummary = ChapterSummary { _getSummary :: M.Map (S.Set ChapterId) (LinkType, ItemList) }
+                         deriving (Show, Eq)
+
+newtype ItemList = ItemList { _getItemList :: M.Map Item Int }
+                   deriving (Show, Eq)
+
+instance Monoid ItemList where
+    mempty = ItemList mempty
+    mappend (ItemList a) (ItemList b) = ItemList (M.unionWith (+) a b)
+
+instance Monoid ChapterSummary where
+    mempty = ChapterSummary mempty
+    mappend (ChapterSummary a) (ChapterSummary b) = ChapterSummary (M.unionWith (<>) a b)
+
+getSummary :: Lens' ChapterSummary (M.Map (S.Set ChapterId) (LinkType, ItemList))
+getSummary f (ChapterSummary s) = ChapterSummary <$> f s
 
 simplify :: [(ChapterId, Chapter)] -> [(ChapterId, Chapter)]
 simplify = IM.toList . simplify' 1 . IM.fromList
 
 data LinkType = Neutral
-              | Good (M.Map Item Int)
+              | Good
               | Bad
               | Both
               deriving (Show, Eq, Ord)
@@ -21,49 +39,60 @@ data LinkType = Neutral
 instance Monoid LinkType where
     mempty = Neutral
     Neutral `mappend` b = b
-    Good a `mappend` Good b = Good (M.unionWith (+) a b)
     a `mappend` Neutral = a
     a `mappend` b = if a == b
                         then a
                         else Both
 
-getLinks :: CharacterConstant -> CharacterVariable -> Decision -> IM.IntMap LinkType
+alt :: ChapterSummary -> ChapterSummary -> ChapterSummary
+alt (ChapterSummary a) (ChapterSummary b) = ChapterSummary $ M.fromListWith (<>) $ do
+   (ca, (la, ia)) <- M.toList a
+   (cb, (lb, ib)) <- M.toList b
+   return (ca <> cb, (la <> lb, ialt ia ib))
+
+ialt :: ItemList -> ItemList -> ItemList
+ialt (ItemList a) (ItemList b) = ItemList (M.intersectionWith min a b)
+
+itemlist :: Item -> Int -> ItemList
+itemlist i n = ItemList (M.singleton i n)
+
+getLinks :: CharacterConstant -> CharacterVariable -> Decision -> ChapterSummary
 getLinks cconstant cvariable d = case d of
-    Decisions decisions       -> IM.unionsWith (<>) (map (getLinks cconstant cvariable . snd) decisions)
-    CanTake i n d'            -> fmap (Good (M.singleton i n) <>) (getLinks cconstant (cvariable & equipment %~ addItem i n) d')
-    Canbuy _ _ d'             -> fmap (Good mempty <>) (getLinks cconstant cvariable d')
-    Cansell _ _ d'            -> fmap (Good mempty <>) (getLinks cconstant cvariable d')
-    Special Cartwheel         -> IM.singleton 136 Both
-    Special Portholes         -> IM.singleton 197 Neutral -- no game
-    EvadeFight _ evadeid _ co -> IM.insertWith (<>) evadeid Bad (fmap (Bad <>) (getLinksO cconstant cvariable co))
+    Decisions decisions       -> foldMap (getLinks cconstant cvariable . snd) decisions
+    CanTake i n d'            -> getLinks cconstant (cvariable & equipment %~ addItem i n) d' & getSummary . traverse <>~ (Good, itemlist i n)
+    Canbuy _ _ d'             -> getLinks cconstant cvariable d' & getSummary . traverse . _1 <>~ Good
+    Cansell _ _ d'            -> getLinks cconstant cvariable d' & getSummary . traverse . _1 <>~ Good
+    Special Cartwheel         -> ChapterSummary (M.singleton (S.singleton 136) (Both, mempty))
+    Special Portholes         -> ChapterSummary (M.singleton (S.singleton 197) (Neutral, mempty)) -- no game
+    EvadeFight _ evadeid _ co -> getLinksO cconstant cvariable co & getSummary . traverse . _1 <>~ Bad & getSummary %~ M.insertWith (<>) (S.singleton evadeid) (Bad, mempty)
     AfterCombat d'            -> getLinks cconstant cvariable d'
     NoDecision o              -> getLinksO cconstant cvariable o
     Conditional c d'          -> if check cconstant cvariable c
-                            then getLinks cconstant cvariable d'
-                            else mempty
+                                     then getLinks cconstant cvariable d'
+                                     else mempty
 
-getLinksO :: CharacterConstant -> CharacterVariable -> ChapterOutcome -> IM.IntMap LinkType
+getLinksO :: CharacterConstant -> CharacterVariable -> ChapterOutcome -> ChapterSummary
 getLinksO cconstant cvariable co = case co of
-    Fight _ o         -> fmap (Bad <>) (go o)
-    Randomly lst      -> IM.unionsWith (<>) (map (go . snd) lst)
+    Fight _ o         -> go o & getSummary . traverse . _1 <>~ Bad
+    Randomly lst      -> foldl1 alt (map (go . snd) lst)
     Conditionally lst -> getLinksO cconstant cvariable (uCheck cconstant cvariable lst)
-    Goto x            -> IM.singleton x Neutral
-    GameLost          -> IM.singleton 1000 Bad
-    GameWon           -> IM.singleton 350 (Good mempty)
-    Simple effects o  -> fmap (foldMap getEffectType effects <>) (getLinksO cconstant cvariable o)
+    Goto x            -> ChapterSummary $ M.singleton (S.singleton x) (Neutral, mempty)
+    GameLost          -> ChapterSummary $ M.singleton mempty (Bad, mempty)
+    GameWon           -> ChapterSummary $ M.singleton mempty (Good, mempty)
+    Simple effects o  -> getLinksO cconstant cvariable o & getSummary . traverse <>~ foldMap getEffectType effects
   where
    go = getLinksO cconstant cvariable
 
-getEffectType :: SimpleOutcome -> LinkType
+getEffectType :: SimpleOutcome -> (LinkType, ItemList)
 getEffectType so = case so of
-    DamagePlayer _ -> Bad
-    HealPlayer _   -> Good mempty
-    HalfHeal       -> Good mempty
-    FullHeal       -> Good mempty
-    LoseItemKind _ -> Bad
-    LoseItem _ _   -> Bad
-    MustEat _      -> Bad
-    GainItem i n   -> Good (M.singleton i n)
+    DamagePlayer _ -> (Bad, mempty)
+    HealPlayer _   -> (Good, mempty)
+    HalfHeal       -> (Good, mempty)
+    FullHeal       -> (Good, mempty)
+    LoseItemKind _ -> (Bad, mempty)
+    LoseItem _ _   -> (Bad, mempty)
+    MustEat _      -> (Bad, mempty)
+    GainItem i n   -> (Good, itemlist i n)
 
 simplify' :: ChapterId -> IM.IntMap Chapter -> IM.IntMap Chapter
 simplify' = undefined getLinks
